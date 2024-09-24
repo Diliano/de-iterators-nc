5b2722f7c9001d8fa46c070218a7c59bcc672054
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-24 19:41:18 +0100

    add functionality and passing tests for the NCEnumerate class

--- src/iterators/enumerator.py
+++ src/iterators/enumerator.py
@@ -1,3 +1,16 @@
 class NCEnumerate:
-    # implement me
-    pass
+    def __init__(self, collection):
+        self.collection_iterator = iter(collection)
+        self.index = 0
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        try:
+            result = (self.index, next(self.collection_iterator))
+            self.index += 1
+            return result
+        except StopIteration:
+            raise StopIteration
+            
--- test/test_enumerator.py
+++ test/test_enumerator.py
@@ -1,5 +1,40 @@
 from src.iterators.enumerator import NCEnumerate
+import pytest
 
 def test_enumerator_is_an_iterator():
-    # implement me
-    pass
+    assert hasattr(NCEnumerate, "__next__")
+
+def test_stores_input_collection_as_collection_iterator_property():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert hasattr(test_iterator.collection_iterator, "__next__")
+
+def test_iter_method_returns_self():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert iter(test_iterator) is test_iterator
+
+def test_next_method_returns_index_and_value_of_next_element():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert next(test_iterator) ==  (0, 1)
+
+def test_next_method_handles_multiple_invocations():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert next(test_iterator) ==  (0, 1)
+    assert next(test_iterator) ==  (1, 2)
+    assert next(test_iterator) ==  (2, 3)
+
+def test_raises_stop_iteration_exception_if_out_of_elements():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert next(test_iterator) ==  (0, 1)
+    assert next(test_iterator) ==  (1, 2)
+    assert next(test_iterator) ==  (2, 3)
+    with pytest.raises(StopIteration):
+        next(test_iterator)
+
+def test_iterator_can_be_converted_to_a_list():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert list(test_iterator) == [(0, 1), (1, 2), (2, 3)]
+
+def test_iterator_can_only_be_iterated_over_once():
+    test_iterator = NCEnumerate([1, 2, 3])
+    assert list(test_iterator) == [(0, 1), (1, 2), (2, 3)]
+    assert list(test_iterator) == []
